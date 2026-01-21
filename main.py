d24313dae554b43012850e920c056f1634124a49
Arker123 <kharbandarnav@gmail.com>
2026-01-21 16:40:36 -0500

    final2

--- main.py
+++ main.py
@@ -16,6 +16,8 @@ robot.end_of_arm.move_to('wrist_yaw',0.5)
 robot.end_of_arm.move_to('wrist_pitch', -0.5)
 robot.end_of_arm.move_to('wrist_roll', 1)
 
+time.sleep(5)
+
 robot.end_of_arm.move_to('stretch_gripper',50)
 robot.end_of_arm.move_to('stretch_gripper',-50)
 
