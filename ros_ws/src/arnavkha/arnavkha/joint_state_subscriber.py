77668d4e7b609715f6947a869db03a85dffecd08
Arker123 <kharbandarnav@gmail.com>
2026-01-24 15:23:44 -0500

    Tweaks

--- ros_ws/src/arnavkha/arnavkha/joint_state_subscriber.py
+++ ros_ws/src/arnavkha/arnavkha/joint_state_subscriber.py
@@ -6,19 +6,19 @@ def main():
     temp.move_to_pose({'joint_arm': 0.5, 'joint_lift': 1.55}, blocking=True)
     # temp.move_to_pose({'joint_lift': (1.55, 40.0)}, custom_contact_thresholds=True)
 
-    temp.move_to_pose({'joint_wrist_yaw': (0.5, 20.0)}, blocking=True)
-    temp.move_to_pose({'joint_wrist_pitch': (-1.0, 20.0)}, blocking=True)
-    temp.move_to_pose({'joint_wrist_roll': (1.0, 20.0)}, blocking=True)
+    temp.move_to_pose({'joint_wrist_yaw': 0.5}, blocking=True)
+    temp.move_to_pose({'joint_wrist_pitch': -1.0}, blocking=True)
+    temp.move_to_pose({'joint_wrist_roll': 1.0}, blocking=True)
 
     # open gripper
-    temp.move_to_pose({'joint_gripper_finger_right': (50, 50)})
-    temp.move_to_pose({'joint_gripper_finger_right': (50, 50)})
-    
+    temp.move_to_pose({'joint_gripper_finger_right': 100}, blocking=True)
+    temp.move_to_pose({'joint_gripper_finger_right': 100}, blocking=True)
+
     # close gripper
-    temp.move_to_pose({'joint_gripper_finger_right': (0, 50)})
-    temp.move_to_pose({'joint_gripper_finger_right': (0, 50)})
-    
-    temp.move_to_pose({'joint_head_pan': (0.0, 20), 'joint_head_tilt': (0.0, 20)})
+    temp.move_to_pose({'joint_gripper_finger_right': 0}, blocking=True)
+    temp.move_to_pose({'joint_gripper_finger_right': 0}, blocking=True)
+
+    temp.move_to_pose({'joint_head_pan': 1.0, 'joint_head_tilt': 1.0}, blocking=True)
     
     # temp.move_to_pose({'translate_mobile_base': 1.0})
     
