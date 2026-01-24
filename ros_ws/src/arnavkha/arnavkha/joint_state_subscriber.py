7549b8e1b47266dea5f413dfc3c8c3e65528a94a
Arker123 <kharbandarnav@gmail.com>
2026-01-24 15:10:32 -0500

    Tweaks

--- ros_ws/src/arnavkha/arnavkha/joint_state_subscriber.py
+++ ros_ws/src/arnavkha/arnavkha/joint_state_subscriber.py
@@ -1,27 +1,26 @@
 import hello_helpers.hello_misc as hm
 
-class joint_state_subscriber:
-    def main():
-        temp = hm.HelloNode.quick_create('temp')
-        temp.stow_the_robot()
-        temp.move_to_pose({'joint_arm': (0.5, 40.0), 'joint_lift': (1.55, 40.0)}, blocking=True)
-        # temp.move_to_pose({'joint_lift': (1.55, 40.0)}, custom_contact_thresholds=True)
+def main():
+    temp = hm.HelloNode.quick_create('temp')
+    temp.stow_the_robot()
+    temp.move_to_pose({'joint_arm': (0.5, 40.0), 'joint_lift': (1.55, 40.0)}, blocking=True)
+    # temp.move_to_pose({'joint_lift': (1.55, 40.0)}, custom_contact_thresholds=True)
 
-        temp.move_to_pose({'joint_wrist_yaw': (0.5, 20.0)}, blocking=True)
-        temp.move_to_pose({'joint_wrist_pitch': (-1.0, 20.0)}, blocking=True)
-        temp.move_to_pose({'joint_wrist_roll': (1.0, 20.0)}, blocking=True)
+    temp.move_to_pose({'joint_wrist_yaw': (0.5, 20.0)}, blocking=True)
+    temp.move_to_pose({'joint_wrist_pitch': (-1.0, 20.0)}, blocking=True)
+    temp.move_to_pose({'joint_wrist_roll': (1.0, 20.0)}, blocking=True)
 
-        # open gripper
-        temp.move_to_pose({'joint_gripper_finger_right': (50, 50)})
-        temp.move_to_pose({'joint_gripper_finger_right': (50, 50)})
-        
-        # close gripper
-        temp.move_to_pose({'joint_gripper_finger_right': (0, 50)})
-        temp.move_to_pose({'joint_gripper_finger_right': (0, 50)})
-        
-        temp.move_to_pose({'joint_head_pan': (0.0, 20), 'joint_head_tilt': (0.0, 20)})
-        
-        # temp.move_to_pose({'translate_mobile_base': 1.0})
-        
-        # temp.move_to_pose({'
-        
+    # open gripper
+    temp.move_to_pose({'joint_gripper_finger_right': (50, 50)})
+    temp.move_to_pose({'joint_gripper_finger_right': (50, 50)})
+    
+    # close gripper
+    temp.move_to_pose({'joint_gripper_finger_right': (0, 50)})
+    temp.move_to_pose({'joint_gripper_finger_right': (0, 50)})
+    
+    temp.move_to_pose({'joint_head_pan': (0.0, 20), 'joint_head_tilt': (0.0, 20)})
+    
+    # temp.move_to_pose({'translate_mobile_base': 1.0})
+    
+    # temp.move_to_pose({'
+    
