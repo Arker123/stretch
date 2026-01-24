ebec566ea786938d106ad40e28f600f6e7642962
Arker123 <kharbandarnav@gmail.com>
2026-01-24 15:04:45 -0500

    Tweaks

--- ros_ws/src/arnavkha/arnavkha/joint_state_subscriber.py
+++ ros_ws/src/arnavkha/arnavkha/joint_state_subscriber.py
@@ -3,12 +3,12 @@ import hello_helpers.hello_misc as hm
 def main():
     temp = hm.HelloNode.quick_create('temp')
     temp.stow_the_robot()
-    temp.move_to_pose({'joint_arm': (0.5, 40.0), 'joint_lift': (1.55, 40.0)}, custom_contact_thresholds=True)
+    temp.move_to_pose({'joint_arm': (0.5, 40.0), 'joint_lift': (1.55, 40.0)}, blocking=True)
     # temp.move_to_pose({'joint_lift': (1.55, 40.0)}, custom_contact_thresholds=True)
-    
-    temp.move_to_pose({'joint_wrist_yaw': (0.5, 20.0)})
-    temp.move_to_pose({'joint_wrist_pitch': (-0.5, 20.0)})
-    temp.move_to_pose({'joint_wrist_roll': (1.0, 20.0)})
+
+    temp.move_to_pose({'joint_wrist_yaw': (0.5, 20.0)}, blocking=True)
+    temp.move_to_pose({'joint_wrist_pitch': (-1.0, 20.0)}, blocking=True)
+    temp.move_to_pose({'joint_wrist_roll': (1.0, 20.0)}, blocking=True)
 
     # open gripper
     temp.move_to_pose({'joint_gripper_finger_right': (50, 50)})
