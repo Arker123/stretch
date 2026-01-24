9acd0c8c4f4d79c8ee66010cab314f40834ffc09
Arker123 <kharbandarnav@gmail.com>
2026-01-24 14:33:10 -0500

    Tweaks

--- main.py
+++ main.py
@@ -10,17 +10,21 @@ robot.arm.move_to(0.5)
 robot.lift.move_to(1.55)
 robot.push_command()
 
+print("Waiting for arm and lift to reach position...")
 time.sleep(5)
 
+print("Moving end effector...")
 robot.end_of_arm.move_to('wrist_yaw',0.5)
 robot.end_of_arm.move_to('wrist_pitch', -0.5)
 robot.end_of_arm.move_to('wrist_roll', 1)
 
 time.sleep(5)
 
+print("Operating gripper...")
 robot.end_of_arm.move_to('stretch_gripper',50)
 robot.end_of_arm.move_to('stretch_gripper',-50)
 
+print("Moving head to neutral position...")
 robot.head.pose('wheels')
 
 # robot.stow()
